3d06a6859dd05b2cd6b6f274228777b5a769ed04
Alex Karadimos <karadalex@gmail.com>
2017-11-26 01:20:51 +0200

    Table for all cards' ids

---
+++ server/database/migrations/2017_11_25_231726_cards.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Cards extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->string('card_id')->unique();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
