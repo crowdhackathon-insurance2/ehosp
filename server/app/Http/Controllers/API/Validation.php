e05505929892eace1c5206756e8aae56e99856f7
Alex Karadimos <karadalex@gmail.com>
2017-11-26 00:38:27 +0200

    Dummy Validation API endpoint

---
+++ server/app/Http/Controllers/API/Validation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Insurance\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Insurance\Http\Controllers\Controller;
+
+class Validation extends Controller
+{
+    public function index()
+    {
+        return 'validation';
+    }
+}
