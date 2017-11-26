918b02cebffd21925fd0006d2fc27a80ea45ade7
Alex Karadimos <karadalex@gmail.com>
2017-11-26 14:50:48 +0200

    Contracts panel

--- server/resources/views/insurance.blade.php
+++ server/resources/views/insurance.blade.php
@@ -10,34 +10,40 @@
                 </div>
 
                 <div class="panel-body">
-                <div class="container">
-  
-             
-  
-</div>
-<table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Κωδικος Συμβολαίου</th>
-        <th>Ονοματεπώνυμο</th>
-        <th>User Id</th>
-        <th>Κόστος</th>
-        <th>Περιγραφή Συμβολαίου</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>AC-5422</td>
-        <td>PAPADOPOULOS NIKOLAOS</td>
-        <td>1234</td>
-        <td>10.000$</td>
-        <td>Πακέτο ατυχήματος: Κάλυψη ιατροφαρμακευτικών εξόδων από ατύχημα, κεφάλαιο σε περίπτωση απώλειας ζωής ή ανικανότητας και βοήθεια στην καθημερινότητα μετά από σοβαρό περιστατικό</td>
-        
-      </tr>
-      
-    </tbody>
-  </table>
+                <div class="container">  
+                    </div>
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Κωδικος Συμβολαίου</th>
+                            <th>Ονοματεπώνυμο</th>
+                            <th>User Id</th>
+                            <th>Κόστος</th>
+                            <th>Περιγραφή Συμβολαίου</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>AC-5422</td>
+                            <td>PAPADOPOULOS NIKOLAOS</td>
+                            <td>1234</td>
+                            <td>10.000$</td>
+                            <td>Πακέτο ατυχήματος: Κάλυψη ιατροφαρμακευτικών εξόδων από ατύχημα, κεφάλαιο σε περίπτωση απώλειας ζωής ή ανικανότητας και βοήθεια στην καθημερινότητα μετά από σοβαρό περιστατικό</td>
+                            
+                        </tr>
+                        
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1> Συμβόλαια </h1>
+                </div>
+                <div class="panel-body">
+                    <button class="btn btn-primary btn-lg" type="button"> Νέο συμβόλαιο </button>
+                    <button class="btn btn-primary btn-lg" type="button"> Ανανέωση συμβολαίου </button>
                 </div>
             </div>
         </div>
