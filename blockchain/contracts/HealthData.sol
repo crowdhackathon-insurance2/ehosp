2d1812f4a578fe2b1e886cf0bd0a2cb8abe48694
Alex Karadimos <karadalex@gmail.com>
2017-11-26 11:15:10 +0200

    Dummy Ethereum Contract

---
+++ blockchain/contracts/HealthData.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.17;
+
+contract HealthData {
+    function printData() returns(string) {
+        return ("Health Data");
+    }
+}
--- blockchain/migrations/1_initial_migration.js
+++ blockchain/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var HealthData = artifacts.require("./HealthData.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(HealthData);
 };
