9a43fc7247f2b0d390fab9c7428ec42448e9af14
Alex Karadimos <karadalex@gmail.com>
2017-11-26 13:42:05 +0200

    Add default values

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $amka = ['18041887678955', '140219905688646'];
+            $blood_types = ['A+', 'B', '0'];
+            $org_don = ['YES', 'NO'];
+            
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('amka');
-            $table->string('blood_type');
-            $table->string('org_don');
+            $table->string('amka')->default($amka[array_rand($amka)]);
+            $table->string('blood_type')->default($blood_types[array_rand($blood_types)]);
+            $table->string('org_don')->default($org_don[array_rand($org_don)]);
             $table->rememberToken();
             $table->timestamps();
         });
