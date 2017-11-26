56bbcc304afd25746910034f645778cb9774aeaf
nikos <nickarad94@gmail.com>
2017-11-26 13:59:49 +0200

    add table view

--- server/resources/views/insurance.blade.php
+++ server/resources/views/insurance.blade.php
@@ -10,7 +10,30 @@
                 </div>
 
                 <div class="panel-body">
-                    
+                <div class="container">
+  <h2>Hover Rows</h2>
+  <p>The .table-hover class enables a hover state on table rows:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Κωδικος Συμβολαίου</th>
+        <th>Ονοματεπώνυμο</th>
+        <th>User Id</th>
+        <th>Κόστος</th>
+        <th>Περιγραφή Συμβολαίου</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>AC-5422</td>
+        <td>PAPADOPOULOS NIKOLAOS</td>
+        <td>1234</td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+
                 </div>
             </div>
         </div>
