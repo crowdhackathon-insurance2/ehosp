1cdf3577277c46a03e6e14b17cf344c5d957dedd
Alex Karadimos <karadalex@gmail.com>
2017-11-26 14:07:01 +0200

    Panel for finance

--- server/resources/views/hospital.blade.php
+++ server/resources/views/hospital.blade.php
@@ -5,35 +5,39 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"> 
-                    <h1> Hospital </h1>
+                <div class="panel-heading">
+                    <h1>Hospital</h1>
                 </div>
-
                 <div class="panel-body">
-                    
                     <table class="table table-striped">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>AMKA</th>
-      <th>Blood Type</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>NIKOLAOS</td>
-      <td>PAPADOPOULOS</td>
-      <td>123456789</td>
-      <td>A+</td>
-    </tr>
-    
-  </tbody>
-</table>
-<button type="button" class="btn btn-primary btn-lg">Αποστολη Τιμολογίου</button>
-
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>AMKA</th>
+                                <th>Blood Type</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>NIKOLAOS</td>
+                                <td>PAPADOPOULOS</td>
+                                <td>123456789</td>
+                                <td>A+</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1> Κοστολόγηση </h1>
+                </div>
+                <div class="panel-body">
+                    <button class="btn btn-primary btn-lg" type="button">Kοστολόγηση Ιατρικών εξόδων</button>
+                    <button class="btn btn-primary btn-lg" type="button">Αποστολη Τιμολογίου</button>
                 </div>
             </div>
         </div>
